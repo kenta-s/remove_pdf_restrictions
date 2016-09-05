e38e2ab3e906f359acca1b75605e42e3d4d91274
Kenta <shirai.kenta@vareal.co.jp>
2016-09-05 21:57:19 +0900

    Module RemovePdfRestrictions

--- lib/remove_pdf_restrictions.rb
+++ lib/remove_pdf_restrictions.rb
@@ -1,5 +1,18 @@
 require "remove_pdf_restrictions/version"
+require 'fileutils'
+require "securerandom"
 
 module RemovePdfRestrictions
-  # Your code goes here...
+
+  def self.execute!(file_name)
+    tmp_file = SecureRandom.uuid
+    File.rename(file_name, tmp_file)
+    begin
+      `gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=#{file_name} -c .setpdfwrite -f #{tmp_file}`
+      FileUtils.rm(tmp_file)
+    rescue => e
+      File.rename(tmp_file, file_name)
+      raise e
+    end
+  end
 end
--- spec/remove_pdf_restrictions_spec.rb
+++ spec/remove_pdf_restrictions_spec.rb
@@ -5,7 +5,10 @@ describe RemovePdfRestrictions do
     expect(RemovePdfRestrictions::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'given not existing file' do
+    it 'raises an error' do
+      not_existing_file = 'foo_bar.pdf'
+      expect{ RemovePdfRestrictions.execute!(not_existing_file) }.to raise_error
+    end
   end
 end
